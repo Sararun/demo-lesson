03c362387188262c5dca5401773c6d6891ba07e0
Sararun <sarosekvlad12@gmail.com>
2022-08-08 21:37:44 +0700

    Маршруты

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
 <?php
+
+declare(strict_types = 1);
+error_reporting(-1);
+session_start();
+require __DIR__ . 'functions/function.php'; //Контроллер
+
+$url = $_SERVER['REQUEST_URI'];
+$parser = parse_url($url);
+$url = trim($parser['path'], '/');
+
+$routes = [
+    [
+        'url' => '#^$|^\?#',
+        'view' => 'sites/index',
+        'action' => 'sites/index',
+    ],
+    [
+        'url' => '#^register?#',
+        'view' => 'auth/register',
+        'action' => 'auth/register',
+    ],
+    [
+        'url' => '#^login?#i',
+        'view' => 'auth/login',
+        'action' => 'auth/login',
+    ]
+];
+
