940d2eb72136ed621486c5c59a5ec77642cfa2cb
Sararun <sarosekvlad12@gmail.com>
2022-08-08 21:53:33 +0700

    проверка логирования

--- functions/auth.php
+++ functions/auth.php
@@ -8,3 +8,20 @@ function register(array $data): string
 
     return !empty($lastId) ? 'success_registry' : 'error_registry';
 }
+
+function login(array $data): array
+{
+    $dbh = connect();
+    $query = "SELECT * FROM users WHERE email=:email LIMIT 1";
+    $sth = $dbh->prepare($query);
+    $sth->execute([':email' => $data['email']]);
+    $result = $sth->fetch();
+
+    if (!empty($result) && password_verify($data['password'], $result['password'])) {
+        $flag = true;
+    } else {
+        $flag = false;
+    }
+
+    return ($flag) ? $result : ['incorrect_login_password'] ;
+}
