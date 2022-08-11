b9bb361e3b5606467f8c4267605212788c62175b
Sararun <sarosekvlad12@gmail.com>
2022-08-11 20:10:19 +0700

    убрал логику ошибок, перенёс в другой файл

--- actions/auth/login.php
+++ actions/auth/login.php
@@ -6,35 +6,20 @@ require __DIR__ . '/../../functions/auth.php'; // логика логирова�
 if (!empty($_POST) && ($_POST['mode'] === 'login_user')) {
     $data = cleanData($_POST);
 
-    $errors = [];
-    if (empty($data['email'])) {
-        $errors['empty_email'] = $massages['auth']['empty_email']['message'];
-    } elseif (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-        $errors['email_not_valid'] = $massages['auth']['email_not_valid']['message'];
-    }
+    $result = login($data);
 
-    if (empty($data['password'])) {
-        $errors['empty_password'] = $massages['auth']['empty_password']['message'];
-    }
-
-    if (!empty($errors)) {
-        $_SESSION['any'] = $errors;
-    } else {
-        $result = login($data);
-        if (!empty($result[0]) && $result[0] === 'incorrect_login_password') {
-            $_SESSION['error'] = $massages['auth'][$result[0]]['message'];
-        } else {
-            foreach ($result as $key => $value) {
-                if ($key != 'password') {
-                    $_SESSION['user'][$key] = $value;
-                }
-            }
-            $_SESSION['success'] = $massages['auth']['success_login']['message'];
-            redirect('/');
+    if (!empty($result['empty_email']) || !empty($result['empty_password']) || !empty($result['incorrect_login_password'])) {
+        foreach ($result as $key => $value) {
+            $_SESSION['any'][$key] = $massages['auth'][$key]['message'];
         }
+        $redirect = '';
+    } else {
+        $_SESSION['user'] = $result;
+        $_SESSION['success'] = $massages['auth']['success']['message'];
+        $redirect = '/';
     }
 
-    redirect();
+    redirect($redirect);
 }
 
 $content = render($view, []);
