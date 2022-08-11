84d81bf850b8054d6eceaf377e31df0fd01b5575
Sararun <sarosekvlad12@gmail.com>
2022-08-12 02:29:57 +0700

    добавил проверку на пустоту в имени товара и чтобы длина названия не превысила 100 символов.

--- actions/backend/categories/create.php
+++ actions/backend/categories/create.php
@@ -10,8 +10,17 @@ if (!empty($_POST) && ($_POST['mode'] === 'add_category')) {
     //сделать проверку на кол-во символов не более 100
     //вывести сообщения об ошибках на клиента
 
+
     $slug = getTranslate($category['name']);
     $params = ['slug' => $slug, 'name' => $category['name']];
+
+    if (empty($category['name'])) {
+        return 'empty_name';
+    }
+
+    if (count($category['name']) >100) {
+        return 'overflow_input';
+    }
 
     if (!empty($category['is_active'])) {
         $params['is_active'] = 1;
