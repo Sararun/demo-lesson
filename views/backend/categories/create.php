eb667a7da36383ccbf2098a8e9eac5e91df2dd7e
Sararun <sarosekvlad12@gmail.com>
2022-08-11 20:59:34 +0700

    Создание

---
+++ views/backend/categories/create.php
@@ -0,0 +1,24 @@
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+            Добавить
+        </div>
+
+        <div class="card-body">
+
+            <div class="text-end">
+                <a href="/admin/categories" class="btn btn-primary" role="button">Назад</a>
+            </div>
+
+            <form method="post">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Название</label>
+                    <input id="name" type="text" value="" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-primary">Сохранить</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+
