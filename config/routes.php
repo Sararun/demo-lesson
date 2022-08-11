1a05cb5c662483d154b91be2c36d291c2a3c9b26
Sararun <sarosekvlad12@gmail.com>
2022-08-11 20:33:40 +0700

    тут будут роуты, из входного файла index убрал

---
+++ config/routes.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    [
+        'url' => '#^$|^\?#',
+        'view' => 'sites/index',
+        'action' => 'sites/index',
+    ],
+    [
+        'url' => '#^register?#i',
+        'view' => 'auth/register',
+        'action' => 'auth/register',
+    ],
+    [
+        'url' => '#^login?#i',
+        'view' => 'auth/login',
+        'action' => 'auth/login',
+    ],
+    [
+        'url' => '#^logout?#i',
+        'view' => '',
+        'action' => 'auth/logout',
+    ],
+
+
+
+    [
+        'url' => '#^admin/categories/create?#i',
+        'view' => 'backend/categories/create',
+        'action' => 'backend/categories/create',
+    ],
+    [
+        'url' => '#^admin/categories?#i',
+        'view' => 'backend/categories/index',
+        'action' => 'backend/categories/index',
+    ],
+    [
+        'url' => '#^admin?#i',
+        'view' => 'backend/sites/index',
+        'action' => 'backend/sites/index',
+    ],
+];
