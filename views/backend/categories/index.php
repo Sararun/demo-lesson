49e9ad7665d5a4ba53e5a6bf435f53aedc2f0898
Sararun <sarosekvlad12@gmail.com>
2022-08-11 21:00:18 +0700

    index

---
+++ views/backend/categories/index.php
@@ -0,0 +1,47 @@
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+            Категории
+        </div>
+
+        <div class="card-body">
+
+            <div class="text-end">
+                <a href="/admin/categories/create" class="btn btn-primary" role="button">Добавить</a>
+            </div>
+
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+
