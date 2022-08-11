b33dfd9919a4538019a889f0bd6b230a50fe65f6
Sararun <sarosekvlad12@gmail.com>
2022-08-12 02:18:24 +0700

    6 строка, исправление орфографии

--- actions/auth/logout.php
+++ actions/auth/logout.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/../../functions/auth.php';
 
 if (!empty($_GET['logout']) && ($_GET['logout']) == 1){
-    $_SESSIOm['success'] = $massages['auth']['success_logout']['message'];
+    $_SESSION['success'] = $massages['auth']['success_logout']['message'];
     logout();
 }
 
