907fce82ffc9cc1328e31dee017e149678440177
Sararun <sarosekvlad12@gmail.com>
2022-08-12 02:15:07 +0700

    Добавил теги

--- views/backend/categories/create.php
+++ views/backend/categories/create.php
@@ -13,12 +13,18 @@
             <form method="post">
                 <div class="mb-3">
                     <label for="name" class="form-label">Название</label>
-                    <input id="name" type="text" value="" class="form-control">
+                    <input id="name" name="name" type="text" value="" class="form-control">
                 </div>
+                <div class="mb-3 form-check">
+                    <input id="is_active" name="is_active" type="checkbox" class="form-check-input">
+                    <label class="form-check-label" for="is_active">Статус</label>
+                </div>
+                <input type="hidden" name="mode" value="add_category">
                 <button type="submit" class="btn btn-primary">Сохранить</button>
             </form>
         </div>
     </div>
 </div>
+
 
 
