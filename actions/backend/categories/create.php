3e2d92f616f436679243b75fdbdae0bdb44e6c9c
Sararun <sarosekvlad12@gmail.com>
2022-08-15 05:36:08 +0700

    Переписал определения ошибок при вводе имя категории в админке

--- actions/backend/categories/create.php
+++ actions/backend/categories/create.php
@@ -4,33 +4,33 @@ require __DIR__ . '/../../../functions/auth.php';
 
 if (!empty($_POST) && ($_POST['mode'] === 'add_category')) {
 
-    if (empty($category['name'])) {
-        return 'empty_name';
-    }
-
-    if (count($category['name']) >100) {
-        return 'overflow_input';
-    }
 
     $category = cleanData($_POST);
 
-    //сделать проверку на пустоту name
-    //сделать проверку на кол-во символов не более 100
-    //вывести сообщения об ошибках на клиента
+    //сделать по аналогии как авторизация и регистрация
+    //то есть переписать в функции которое добавлить в файл categories.php folder function
 
+    $redirect = 'admin/categories/create';
 
-    $slug = getTranslate($category['name']);
-    $params = ['slug' => $slug, 'name' => $category['name']];
+    //  Если имя категории не введено
+    if (empty($category['name'])) {
+        $_SESSION['error'] = $messages['categories']['empty_name']['message'];
 
+        //  Если имя категории длиннее 100 символов
+    } elseif (mb_strlen($category['name']) > 100) {
+        $_SESSION['error'] = $messages['categories']['big_name']['message'];
+    } else {
+        $slug = getTranslate($category['name']);
+        $params = ['slug' => $slug, 'name' => $category['name']];
 
+        if (!empty($category['is_active'])) {
+            $params['is_active'] = 1;
+        }
 
-    if (!empty($category['is_active'])) {
-        $params['is_active'] = 1;
-    }
-
-    if (insert('categories', $params)) {
-        $_SESSION['success'] = $massages['add'];
-//        redirect('/admin/categories');
+        if (insert('categories', $params)) {
+            $_SESSION['success'] = $messages['add'];
+            $redirect = '/admin/categories';
+        }
     }
 
 }
