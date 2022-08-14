ca8c16a89e9657898d9500cb63ef2bc56e762810
Sararun <sarosekvlad12@gmail.com>
2022-08-15 05:41:09 +0700

    Файл добавления записей к категориям в бд. Потом на другие таблицы будет также, а потом в ооп один большой файл запишем

--- actions/backend/categories/index.php
+++ actions/backend/categories/index.php
@@ -1,3 +1,14 @@
 <?php
 
-$content = render($view);
+//подключить файл категории функциии
+
+$dbh = connect();
+$query = "SELECT * FROM `categories` ORDER BY di DESC";
+$sth = $dbh->prepare($query);
+$sth->execute();
+$categories = $sth->fetchAll();
+
+$content = render($view, [
+    'categories' => $categories,
+]);
+
