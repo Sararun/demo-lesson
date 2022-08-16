409e4de8dfb95b691ad0f3d98900463ceeb9b4e6
Sararun <sarosekvlad12@gmail.com>
2022-08-16 20:30:34 +0700

    ДОбернул всё в функции

--- functions/categories.php
+++ functions/categories.php
@@ -1,57 +1,53 @@
 <?php
-function create() {
-    if (!empty($_POST) && ($_POST['mode'] === 'add_category'))
-    {
 
-        $category = cleanData($_POST);
-        //сделать по аналогии как авторизация и регистрация
-        //то есть переписать в функции которое добавлить в файл categories.php folder function
-
-        $redirect = '/admin/categories/create';
-        if (empty($category['name'])) {
-            $_SESSION['error'] = $messages['categories']['empty_name']['message'];
-
-        } elseif (mb_strlen($category['name']) > 100) {
-            $_SESSION['error'] = $messages['categories']['big_name']['message'];
-        } else {
-            $slug = getTranslate($category['name']);
-            $params = ['slug' => $slug, 'name' => $category['name']];
-
-            if (!empty($category['is_active'])) {
-                $params['is_active'] = 1;
-            }
-
-            if (insert('categories', $params)) {
-                $_SESSION['success'] = $messages['add'];
-                $redirect = '/admin/categories';
-            }
-        }
-        redirect($redirect);
-    }
-
-    $content = render($view);
-}
-
-function update(string $tableName, array $data): bool
+function getOneCategory(int $id): ?array
 {
-    $columns = [];
-    $values = [':id' => $data['id']];
-    foreach ($data as $key => $value) {
-        if ($key == 'id') {
-            continue;
-        }
-        $columns[] = "{$key}=:{$key}";
-        $values[":{$key}"] = $value;
-    }
-
-    $query = "UPDATE `{$tableName}` SET "
-        . implode(', ', $columns)
-        . " WHERE `id`=:id LIMIT 1";
-
+    $query = "SELECT * FROM `categories` WHERE id=:id LIMIT 1";
     $dbh = connect();
     $sth = $dbh->prepare($query);
-    $sth->execute($values);
-    $result = $sth->rowCount();
+    $sth->execute([':id' => $id]);
+    $result = $sth->fetch();
 
-    return (bool)$result;
+    return ($result !== false) ? $result : null;
+}
+
+function validateCategoryData(array $data): ?string
+{
+    if (empty($data['name'])) {
+        return 'empty_name';
+    } elseif (mb_strlen($data['name']) > 100) {
+        return 'big_name';
+    }
+
+    return null;
+}
+
+function createCategory(array $data): int
+{
+    $slug = getTranslate($data['name']);
+    $params = ['slug' => $slug, 'name' => $data['name']];
+
+    if (!empty($data['is_active'])) {
+        $params['is_active'] = 1;
+    }
+
+    $lastId = insert('categories', $params);
+
+    return $lastId;
+}
+
+function updateCategory(array $data): bool
+{
+    $slug = getTranslate($data['name']);
+    $params = ['id' => $data['id'], 'slug' => $slug, 'name' => $data['name']];
+
+    if (!empty($data['is_active'])) {
+        $params['is_active'] = 1;
+    } else {
+        $params['is_active'] = 0;
+    }
+
+    $result = update('categories', $params);
+
+    return $result;
 }
