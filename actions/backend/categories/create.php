0b2fbbf583ff18ea4fdbeaa9eaed515077753098
Sararun <sarosekvlad12@gmail.com>
2022-08-16 20:25:45 +0700

    Переписал в функцию functions/categories.php

--- actions/backend/categories/create.php
+++ actions/backend/categories/create.php
@@ -1,33 +1,24 @@
 <?php
 
-//подключить файл категории функциии
 require __DIR__ . '/../../../functions/categories.php';
 
 if (!empty($_POST) && ($_POST['mode'] === 'add_category')) {
 
-    $category = cleanData($_POST);
-    //сделать по аналогии как авторизация и регистрация
-    //то есть переписать в функции которое добавлить в файл categories.php folder function
+    $data = cleanData($_POST);
+
+    $nameError = validateCategoryData($data);
 
     $redirect = '/admin/categories/create';
-    if (empty($category['name'])) {
-        $_SESSION['error'] = $messages['categories']['empty_name']['message'];
 
-    } elseif (mb_strlen($category['name']) > 100) {
-        $_SESSION['error'] = $messages['categories']['big_name']['message'];
-    } else {
-        $slug = getTranslate($category['name']);
-        $params = ['slug' => $slug, 'name' => $category['name']];
-
-        if (!empty($category['is_active'])) {
-            $params['is_active'] = 1;
-        }
-
-        if (insert('categories', $params)) {
-            $_SESSION['success'] = $messages['add'];
-            $redirect = '/admin/categories';
-        }
+    if (!is_null($nameError)) {
+        $_SESSION['error'] = $messages['categories'][$nameError]['message'];
+    } elseif (checkTableName('categories', 'slug', getTranslate($data['name']))) {
+        $_SESSION['error'] = $messages['categories']['slug_busy']['message'];
+    } elseif (createCategory($data)) {
+        $_SESSION['success'] = $messages['add'];
+        $redirect = '/admin/categories';
     }
+
     redirect($redirect);
 }
 
