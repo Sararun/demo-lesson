0889589fe0c5b3b7b6e3b63aed59234c549c7ec7
Sararun <sarosekvlad12@gmail.com>
2022-08-16 20:27:22 +0700

    Переписал для работы с функциями

--- actions/backend/categories/update.php
+++ actions/backend/categories/update.php
@@ -1,33 +1,20 @@
 <?php
-//подключить файл категории функциии
+
+require __DIR__ . '/../../../functions/categories.php';
 
 if (!empty($_POST) && ($_POST['mode'] === 'update_category')) {
 
-    $category = cleanData($_POST);
+    $data = cleanData($_POST);
 
-    //сделать по аналогии как авторизация и регистрация
-    //то есть переписать в функции которое добавлить в файл categories.php folder function
+    $nameError = validateCategoryData($data);
 
-    $redirect = '/admin/categories/create';
-    if (empty($category['name'])) {
-        $_SESSION['error'] = $messages['categories']['empty_name']['message'];
+    $redirect = '/admin/categories/update?id=' . $data['id'];
 
-    } elseif (mb_strlen($category['name']) > 100) {
-        $_SESSION['error'] = $messages['categories']['big_name']['message'];
-    } else {
-        $slug = getTranslate($category['name']);
-        $params = ['id' => $category['id'], 'slug' => $slug, 'name' => $category['name']];
-
-        if (!empty($category['is_active'])) {
-            $params['is_active'] = 1;
-        } else {
-            $params['is_active'] = 0;
-        }
-
-        if (update('categories', $params)) {
-            $_SESSION['success'] = $messages['update'];
-            $redirect = '/admin/categories';
-        }
+    if (!is_null($nameError)) {
+        $_SESSION['error'] = $messages['categories'][$nameError]['message'];
+    } elseif (updateCategory($data)) {
+        $_SESSION['success'] = $messages['update'];
+        $redirect = '/admin/categories';
     }
 
     redirect($redirect);
@@ -41,11 +28,7 @@ if (empty($id)) {
     die;
 }
 
-$query = "SELECT * FROM `categories` WHERE id=:id LIMIT 1";
-$dbh = connect();
-$sth = $dbh->prepare($query);
-$sth->execute([':id' => $id]);
-$category = $sth->fetch();
+$category = getOneCategory($id);
 
 if (empty($category)) {
     http_response_code(404);
